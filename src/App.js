8735f0cf2909874c8e230887771866a14c0d9bf1
Luisfer Cordova <lcordova@paystand.com>
2024-12-27 09:38:57 -0700

    remove extra css files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './styles/App.css';
+import './App.css';
 import Header from './components/Header';
 import About from './components/About';
 import Experience from './components/Experience';
